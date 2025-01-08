da5da9e4a4cd44e7b7241dab3e2241fd356bdb9c
coutooo <manuelcouto10@ua.pt>
2025-01-08 15:12:55 +0000

    new UI

---
+++ script.js
@@ -0,0 +1,21 @@
+document.getElementById('read-more-btn').addEventListener('click', function() {
+  var description = document.getElementById('medtracking-description');
+  if (description.classList.contains('expanded')) {
+    description.classList.remove('expanded');
+    this.textContent = 'Read More';
+  } else {
+    description.classList.add('expanded');
+    this.textContent = 'Read Less';
+  }
+});
+
+document.getElementById('read-more-btn-v2x').addEventListener('click', function() {
+  var description = document.getElementById('v2x-description');
+  if (description.classList.contains('expanded')) {
+    description.classList.remove('expanded');
+    this.textContent = 'Read More';
+  } else {
+    description.classList.add('expanded');
+    this.textContent = 'Read Less';
+  }
+});
